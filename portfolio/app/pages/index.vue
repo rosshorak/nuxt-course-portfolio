7acb421cacb097d207a15b0ad19b31d2c331e270
rosshorak <hi@rosshorak.com>
2025-01-12 21:10:07 +0200

    Add highlighter notice on home page - PR request fun

--- portfolio/app/pages/index.vue
+++ portfolio/app/pages/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <article class="prose dark:prose-invert max-w-none">
-    <ContentDoc path="/" />
-  </article>
+    <article class="prose dark:prose-invert max-w-none">
+        <div class="my-12 highlight inline-block">
+            Messing around with pull requests and changes
+        </div>
+        <ContentDoc path="/"/>
+    </article>
 </template>
+
+<style>
+.highlight {
+    background-color: yellow;
+    padding: 5px 15px;
+    border-radius: 8px;
+}
+</style>
